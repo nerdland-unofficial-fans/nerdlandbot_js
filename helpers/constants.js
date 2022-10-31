cde0ea3d841a89e971c8448d15c3ba911aa712d0
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-10-31 16:21:03 +0100

    add wombat command

--- helpers/constants.js
+++ helpers/constants.js
@@ -9,5 +9,6 @@ module.exports = {
     API_FETCH_LIMIT: 100, // 100 is the discord API limit SADFACE.JPG
     BULK_DELETE_MAX_AGE: 14 * 24 * 60 * 60 * 1000 // 2 weeks in milliseconds
   },
-  CRON_REGEX_SYNTAX: /(((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5}/
+  CRON_REGEX_SYNTAX: /(((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5}/,
+  WOMBATS_DIR_NAME: 'wombats',
 }
