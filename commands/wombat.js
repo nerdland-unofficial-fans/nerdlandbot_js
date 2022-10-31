e5fb96bd78736a0af60fc5b17682ac3a5c5cd956
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-10-31 18:55:09 +0100

    fix formattig

--- commands/wombat.js
+++ commands/wombat.js
@@ -1,32 +1,32 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { reply } = require("../helpers/interactionHelper");
-const { WOMBATS_DIR_NAME } = require("../helpers/constants");
-const path = require("path");
-const fs = require("fs/promises");
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { reply } = require('../helpers/interactionHelper')
+const { WOMBATS_DIR_NAME } = require('../helpers/constants')
+const path = require('path')
+const fs = require('fs/promises')
 
 const data = new SlashCommandBuilder()
-  .setName("wombat")
-  .setDescription("Post een willekeurige foto van een wombat.");
+  .setName('wombat')
+  .setDescription('Post een willekeurige foto van een wombat.')
 
-async function execute(interaction) {
-  let wombatList;
+async function execute (interaction) {
+  let wombatList
   try {
-    wombatList = await fs.readdir(path.resolve(WOMBATS_DIR_NAME));
+    wombatList = await fs.readdir(path.resolve(WOMBATS_DIR_NAME))
 
     if (wombatList.length === 0) {
-      throw new Error("Map met foto's is leeg");
+      throw new Error("Map met foto's is leeg")
     }
   } catch (error) {
-    return reply(interaction, "Er zijn geen wombat foto's gevonden.");
+    return reply(interaction, "Er zijn geen wombat foto's gevonden.")
   }
 
-  const wombatPhoto = wombatList[Math.floor(Math.random() * wombatList.length)];
+  const wombatPhoto = wombatList[Math.floor(Math.random() * wombatList.length)]
   return reply(interaction, {
-    files: [path.resolve(WOMBATS_DIR_NAME, wombatPhoto)],
-  });
+    files: [path.resolve(WOMBATS_DIR_NAME, wombatPhoto)]
+  })
 }
 
 module.exports = {
   data,
-  execute,
-};
+  execute
+}
