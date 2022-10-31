8c17170f32cc65e49a3179e6a975f12edf680fc5
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-10-31 16:09:18 +0100

    remove python code comments

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -3,17 +3,6 @@ const { reply } = require('../helpers/interactionHelper')
 const { DAD_JOKE_URL } = require('../helpers/constants')
 const axios = require('axios')
 
-// @commands.command(name='dad_joke', aliases=['dadjoke'], help='dad_joke_help',no_pm = True)
-// @commands.guild_only()
-// async def cmd_dad_joke(self, ctx: commands.Context):
-//     async with aiohttp.ClientSession() as session:
-//         async with session.get(DAD_JOKE_URL,headers = { 'Accept': 'text/plain' }) as resp:
-//             msg = await resp.text(encoding='utf-8')
-
-//             # msg can contain utf 2028 charactercode which is line-separator, we're just converting it to '\n'
-//             msg = '\n'.join(msg.splitlines())
-//             await ctx.send(msg)
-
 const data = new SlashCommandBuilder()
   .setName('dadjoke')
   .setDescription('Geeft een al dan niet flauwe mop')
