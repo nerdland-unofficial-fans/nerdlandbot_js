f3b4bf72c80e4d58d613f10cccefe4509c9597c5
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-11-06 16:39:35 +0100

    Move data and execute to module.exports

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -3,24 +3,20 @@ const { reply } = require('../helpers/interactionHelper')
 const { DAD_JOKE_URL } = require('../helpers/constants')
 const axios = require('axios')
 
-const data = new SlashCommandBuilder()
-  .setName('dadjoke')
-  .setDescription('Geeft een al dan niet flauwe mop')
-
-async function execute (interaction) {
-  const joke = (
-    await axios.get(DAD_JOKE_URL, {
-      headers: { Accept: 'text/plain', 'User-Agent': 'axios 0.21.1' }
-    })
-  ).data
-
-  await reply(interaction, {
-    content: joke,
-    allowedMentions: { repliedUser: false }
-  })
-}
-
 module.exports = {
-  data,
-  execute
+  data: new SlashCommandBuilder()
+    .setName('dadjoke')
+    .setDescription('Geeft een al dan niet flauwe mop'),
+  async execute (interaction) {
+    const joke = (
+      await axios.get(DAD_JOKE_URL, {
+        headers: { Accept: 'text/plain', 'User-Agent': 'axios 0.21.1' }
+      })
+    ).data
+
+    await reply(interaction, {
+      content: joke,
+      allowedMentions: { repliedUser: false }
+    })
+  }
 }
