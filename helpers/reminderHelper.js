1ad7dc36ead0f76fad829b511d72a378d363856b
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2022-11-12 18:15:34 +0100

    #5 - Add input validation

--- helpers/reminderHelper.js
+++ helpers/reminderHelper.js
@@ -1,10 +1,14 @@
 const { getGuild, saveGuild } = require('./guildData')
 const { discordTime } = require('./DateTimeHelper')
+const { foemp } = require('./foemp')
 
 async function setReminder (interaction) {
   const guild = await getGuild(interaction.guildId)
   const currentTime = discordTime().toEpochSecond().toString()
   const reminderMinutes = interaction.fields.getTextInputValue('reminderTime')
+  if (isNaN(reminderMinutes)) {
+    return await interaction.reply({ content: `Je hebt het aantal minuten ingesteld (${reminderMinutes}) in een formaat dat ik niet begrijp. Probeer een integer (bv. 10), ${foemp()}!`, ephemeral: true })
+  }
   const timeToBeReminded = discordTime().plusMinutes(interaction.fields.getTextInputValue('reminderTime')).toEpochSecond()
   const message = interaction.fields.getTextInputValue('reminderMessage')
   const memberId = interaction.member.id
