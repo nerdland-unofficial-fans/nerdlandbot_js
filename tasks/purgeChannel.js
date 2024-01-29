a6822d6831f4cd8af24ed1599e06b908393d155e
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2024-01-29 21:59:53 +0100

    Use new for new CronJob syntax

--- tasks/purgeChannel.js
+++ tasks/purgeChannel.js
@@ -53,7 +53,7 @@ function removePurgeChannelTask (purger) {
 }
 
 function addPurgerAndStartTask (purger) {
-  tasks[purger.channelId] = cron.job(
+  tasks[purger.channelId] = new cron.CronJob(
     purger.cronTime,
     async function () {
       try {
